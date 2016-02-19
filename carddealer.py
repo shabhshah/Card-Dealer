b41e6a171d29345fbf55d78051cb97746912f4c2
Rishabh <rishabhshah28@gmail.com>
2016-02-19 14:12:43 -0500

    Cleaned up the code.

--- carddealer.py
+++ carddealer.py
@@ -57,7 +57,19 @@ blankLine()
 
 while True:
 	#deck of cards in a list
-	possible_cards = ['2 of diamonds', '3 of diamonds', '4 of diamonds', '5 of diamonds', '6 of diamonds', '7 of diamonds', '8 of diamonds', '9 of diamonds', '10 of diamonds', 'Jack of diamonds', 'Queen of diamonds', 'King of diamonds', 'Ace of diamonds', '2 of hearts', '3 of hearts', '4 of hearts', '5 of hearts', '6 of hearts', '7 of hearts', '8 of hearts', '9 of hearts', '10 of hearts', 'Jack of hearts', 'Queen of hearts', 'King of hearts', 'Ace of hearts', '2 of spades', '3 of spades', '4 of spades', '5 of spades', '6 of spades', '7 of spades', '8 of spades', '9 of spades', '10 of spades', 'Jack of spades', 'Queen of spades', 'King of spades', 'Ace of spades', '2 of clubs', '3 of clubs', '4 of clubs', '5 of clubs', '6 of clubs', '7 of clubs', '8 of clubs', '9 of clubs', '10 of clubs', 'Jack of clubs', 'Queen of clubs', 'King of clubs', 'Ace of clubs']
+	possible_cards = ['2 of diamonds', '3 of diamonds', '4 of diamonds', '5 of diamonds', \
+					 '6 of diamonds', '7 of diamonds', '8 of diamonds', '9 of diamonds', \
+					 '10 of diamonds', 'Jack of diamonds', 'Queen of diamonds', 'King of diamonds', \
+					 'Ace of diamonds', '2 of hearts', '3 of hearts', '4 of hearts', '5 of hearts', \
+					 '6 of hearts', '7 of hearts', '8 of hearts', '9 of hearts', \
+					 '10 of hearts', 'Jack of hearts', 'Queen of hearts', 'King of hearts', \
+					 'Ace of hearts', '2 of spades', '3 of spades', '4 of spades', \
+					 '5 of spades', '6 of spades', '7 of spades', '8 of spades', \
+					 '9 of spades', '10 of spades', 'Jack of spades', 'Queen of spades', \
+		  		 	 'King of spades', 'Ace of spades', '2 of clubs', '3 of clubs', \
+				 	 '4 of clubs', '5 of clubs', '6 of clubs', '7 of clubs', \
+					 '8 of clubs', '9 of clubs', '10 of clubs', 'Jack of clubs', \
+					 'Queen of clubs', 'King of clubs', 'Ace of clubs']
 	#asking how many hands and cards in each hand
 	while True:
 		while True:
